184ffbd2a029cf69492417e9e3534db02190e32c
Thisoftware <yaoliang1220@163.com>
2017-08-10 20:07:18 +0800

    change .git

---
+++ src/main/java/com/xxx/Application.java
@@ -0,0 +1,18 @@
+package com.xxx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by wangyaoliang on 2017-06-.13
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(Application.class,args);
+
+    }
+
+}
